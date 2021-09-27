d883fab92031bbcc95e8588980be0dab628d49da
1-alimousavi-1 <91486758+1-alimousavi-1@users.noreply.github.com>
2021-09-27 09:28:51 -0700

    Add files via upload

---
+++ DotNetDownloader/MyApplication/frmNewDownload.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace MyApplication
+{
+    partial class frmNewDownload
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtDownloadLink = new System.Windows.Forms.TextBox();
+            this.btnStartDownload = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtPath = new System.Windows.Forms.TextBox();
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.exit = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.label1.Font = new System.Drawing.Font("Tahoma", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(634, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(93, 21);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "لینک دانلود:";
+            // 
+            // txtDownloadLink
+            // 
+            this.txtDownloadLink.Location = new System.Drawing.Point(33, 22);
+            this.txtDownloadLink.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txtDownloadLink.Multiline = true;
+            this.txtDownloadLink.Name = "txtDownloadLink";
+            this.txtDownloadLink.Size = new System.Drawing.Size(576, 32);
+            this.txtDownloadLink.TabIndex = 1;
+            // 
+            // btnStartDownload
+            // 
+            this.btnStartDownload.BackColor = System.Drawing.Color.GreenYellow;
+            this.btnStartDownload.Location = new System.Drawing.Point(33, 147);
+            this.btnStartDownload.Name = "btnStartDownload";
+            this.btnStartDownload.Size = new System.Drawing.Size(184, 53);
+            this.btnStartDownload.TabIndex = 2;
+            this.btnStartDownload.Text = "اضافه به لیست دانلود";
+            this.btnStartDownload.UseVisualStyleBackColor = false;
+            this.btnStartDownload.Click += new System.EventHandler(this.btnStartDownload_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.label2.Font = new System.Drawing.Font("Tahoma", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(634, 66);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(75, 21);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "ذخیره در:";
+            // 
+            // txtPath
+            // 
+            this.txtPath.Location = new System.Drawing.Point(136, 64);
+            this.txtPath.Multiline = true;
+            this.txtPath.Name = "txtPath";
+            this.txtPath.Size = new System.Drawing.Size(473, 32);
+            this.txtPath.TabIndex = 4;
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.BackColor = System.Drawing.Color.Cyan;
+            this.btnBrowse.Location = new System.Drawing.Point(33, 64);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(97, 35);
+            this.btnBrowse.TabIndex = 5;
+            this.btnBrowse.Text = "جستجو...";
+            this.btnBrowse.UseVisualStyleBackColor = false;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // exit
+            // 
+            this.exit.BackColor = System.Drawing.Color.Red;
+            this.exit.Location = new System.Drawing.Point(609, 154);
+            this.exit.Name = "exit";
+            this.exit.Size = new System.Drawing.Size(114, 38);
+            this.exit.TabIndex = 6;
+            this.exit.Text = "خروج";
+            this.exit.UseVisualStyleBackColor = false;
+            this.exit.Click += new System.EventHandler(this.exit_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 292);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(428, 21);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "instagram= 1_alimousavi_1  //   telegram=alimousavi_1";
+            // 
+            // frmNewDownload
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 19F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.PeachPuff;
+            this.ClientSize = new System.Drawing.Size(750, 321);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.exit);
+            this.Controls.Add(this.btnBrowse);
+            this.Controls.Add(this.txtPath);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnStartDownload);
+            this.Controls.Add(this.txtDownloadLink);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.MaximizeBox = false;
+            this.Name = "frmNewDownload";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "ایجاد دانلود جدید";
+            this.Load += new System.EventHandler(this.frmNewDownload_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtDownloadLink;
+        private System.Windows.Forms.Button btnStartDownload;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtPath;
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.Button exit;
+        private System.Windows.Forms.Label label3;
+    }
+}
---
+++ DotNetDownloader/MyApplication/frmNewDownload.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Windows.Forms;
+
+namespace MyApplication
+{
+    public partial class frmNewDownload : Form
+    {//coded by =alimousavi    instagram= 1_alimousavi_1  telegram=alimousavi_1
+        public frmMain MainForm { get; set; }
+
+        public frmNewDownload(frmMain mainForm)
+        {
+            MainForm = mainForm;
+
+            InitializeComponent();
+        }
+
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {//gui
+            SaveFileDialog saveFile = new SaveFileDialog();
+            saveFile.Title = "لطفا محل ذخیره سازی فایل را انتخاب کنید";
+            saveFile.FileName = Path.GetFileName(txtDownloadLink.Text);
+            saveFile.ShowDialog();
+            txtPath.Text = saveFile.FileName;
+        }
+
+        /*private void ckhDownloadLater_CheckedChanged(object sender, EventArgs e)
+        {
+            if (chkDownloadLater.Checked == true)
+            {//gui
+                txtTime.Enabled = true;
+                txtDate.Enabled = true;
+            }//coded by =alimousavi    instagram= 1_alimousavi_1  telegram=alimousavi_1
+            else
+            {
+                txtTime.Enabled = false;
+                txtDate.Enabled = false;
+            }
+        }*/
+
+        private void frmNewDownload_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                
+
+                if (Clipboard.ContainsText())
+                {
+                    Uri fileuri = new Uri(Clipboard.GetText());
+
+                    if(fileuri.Scheme == Uri.UriSchemeHttp || fileuri.Scheme == Uri.UriSchemeHttps)
+                        txtDownloadLink.Text = Clipboard.GetText();
+                }
+            }//coded by =alimousavi    instagram= 1_alimousavi_1  telegram=alimousavi_1
+            catch { }
+        }
+
+        private void btnStartDownload_Click(object sender, EventArgs e)
+        {
+            if (txtDownloadLink.Text != "")
+            {
+                //Download Codes
+                if (txtPath.Text != "")
+                {
+                    MainForm.SetListViewItems(txtDownloadLink.Text, txtPath.Text);
+
+                    (new frmDownloading(MainForm, txtDownloadLink.Text, txtPath.Text)).Show();
+                    Close();
+                }//coded by =alimousavi    instagram= 1_alimousavi_1 telegram=alimousavi_1
+                else
+                    MessageBox.Show("محل ذخیره سازی فایل انتخاب نشده است", "خطا", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+                MessageBox.Show("لینک دانلود فایل خالی می باشد", "خطا", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+
+        private void exit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
